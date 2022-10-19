8604211d5cf2a877295c3ab6b1c767711608824a
serhio107 <65783306+serhio107@users.noreply.github.com>
2022-10-19 17:36:38 +0200

    shoot the ray out of me
    shooting implemented, the objects can take damage from the ray, also visual improvement on the ray

--- MiniPlanet/Assets/mining/Mining.cs
+++ MiniPlanet/Assets/mining/Mining.cs
@@ -2,30 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Mining : MonoBehaviour
 {
-
-    public float health;
-    private float currentHealth = 0;
-    // Start is called before the first frame update
-    void Start()
+    
+    public float health = 50f;
+    //i will let it public for now, different minerals will have different health
+    public void TakeDamage(float amount)
+    //im creating the take damage value, the mining will work like this(the iron will have 50 health point and it will take damage over time from the laser)
     {
-        currentHealth = health;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    public void DamageDealt(float amount)
-    {
-        currentHealth -= amount;
-        if (currentHealth <= 0)
-        {
-            gameObject.SetActive(false);
+        health -= amount;
+        if (health <= 0f)
+        {   //if the health is below 0 the rock will explode and the player will be able to collet materials 
+            Explode();
+            
         }
 
     }
+
+    void Explode()
+    {
+        Destroy(gameObject);
+    }
+
 }
