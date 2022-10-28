d178f1de1f8da17652975440bf201a54369adb33
CodringherAndFenn <ivanovjucanmihai@gmail.com>
2022-10-28 15:15:28 +0200

    hghgtgh

--- GizmosPlsWork/Assets/Scripts/PlayerMovement2D.cs
+++ GizmosPlsWork/Assets/Scripts/PlayerMovement2D.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement2D : MonoBehaviour
 {
     public PlayerController controller;
+    public Animator animator;
 
     [SerializeField] float horizontalMove;
     [SerializeField] float runSpeed;
@@ -16,14 +17,22 @@ public class PlayerMovement2D : MonoBehaviour
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
+            animator.SetBool("IsJumping", true);
         }
         if (Input.GetButtonDown("Crouch"))
         {
             crouch = true;
         } else if (Input.GetButtonUp("Crouch")) crouch = false;
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
     }
 
     private void FixedUpdate()
