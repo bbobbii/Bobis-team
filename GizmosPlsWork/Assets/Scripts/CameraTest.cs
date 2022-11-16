2b082765490f2ef9901872a50e9ef4386d48cb5d
CodringherAndFenn <ivanovjucanmihai@gmail.com>
2022-11-16 15:10:25 +0100

    lmao

---
+++ GizmosPlsWork/Assets/Scripts/CameraTest.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTest : MonoBehaviour
+{
+
+    public GameObject MainCamera;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
