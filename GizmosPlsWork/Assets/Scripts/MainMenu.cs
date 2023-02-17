b23d48a387262cb6a214230ca6c70f430e154fd5
CodringherAndFenn <ivanovjucanmihai@gmail.com>
2023-02-17 12:44:40 +0100

    ok

---
+++ GizmosPlsWork/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit this shit");
+        Application.Quit();
+    }
+
+    private void Start()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                //SceneManager.LoadScene(2);
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("PauseScene");
+    }
+}
